d17e4d27f02b2fd2dedc9be4d92136df7b867341
“Achraf <achrafassouam@gmail.com>
2024-09-26 22:57:09 +0100

    0-island_perimeter.py

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -3,29 +3,28 @@
 define the perimeter of the island described in grid
 """
 
+
 def island_perimeter(grid):
-	perimiter = 0
-	rows = len(grid)
-	cols = len(grid[0])
+    perimiter = 0
+    rows = len(grid)
+    cols = len(grid[0])
 
-	for r in range(rows):
-		for c in range(cols):
-			if grid[r][c] == 1:
-				# full perimeter of 4 for a land cell
-				perimiter += 4
+    for r in range(rows):
+        for c in range(cols):
+            if grid[r][c] == 1:
 
-				#Check for neighboring land cells
-				#up
-				if r > 0 and grid[r-1][c] == 1:
-					perimiter -= 1
-				#down
-				if r < rows - 1 and grid[r+1][c] == 1:
-					perimiter -= 1
-				#left
-				if c > 0 and grid[r][c-1] == 1:
-					perimiter -= 1
-				#Right
-				if c < cols and grid[r][c+1] == 1:
-					perimiter -= 1
-	
-	return perimiter
+                perimiter += 4
+
+                if r > 0 and grid[r-1][c] == 1:
+                    perimiter -= 1
+
+                if r < rows - 1 and grid[r+1][c] == 1:
+                    perimiter -= 1
+
+                if c > 0 and grid[r][c-1] == 1:
+                    perimiter -= 1
+
+                if c < cols and grid[r][c+1] == 1:
+                    perimiter -= 1
+
+    return perimiter
