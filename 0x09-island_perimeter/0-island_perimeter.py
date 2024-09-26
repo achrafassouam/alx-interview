37c07bee00b40ac354c087b81c7f2f72f7633780
“Achraf <achrafassouam@gmail.com>
2024-09-26 23:05:07 +0100

    0-island_perimeter.py

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -8,26 +8,17 @@ def island_perimeter(grid):
     """
     return the perimeter
     """
-    perimiter = 0
-    rows = len(grid)
     cols = len(grid[0])
+    rows = len(grid)
+    size = 0
+    edge = 0
 
     for r in range(rows):
         for c in range(cols):
             if grid[r][c] == 1:
-
-                perimiter += 4
-
-                if r > 0 and grid[r-1][c] == 1:
-                    perimiter -= 1
-
-                if r < rows - 1 and grid[r+1][c] == 1:
-                    perimiter -= 1
-
-                if c > 0 and grid[r][c-1] == 1:
-                    perimiter -= 1
-
-                if c < cols and grid[r][c+1] == 1:
-                    perimiter -= 1
-
-    return perimiter
+                size += 1
+                if (c > 0 and grid[r][c - 1] == 1):
+                    edge += 1
+                if (r > 0 and grid[r - 1][c] == 1):
+                    edge += 1
+    return size * 4 - edge * 2
